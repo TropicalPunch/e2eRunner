d49f35a28626b88e4df41a3b365163ac4eb19e15
Daniel Huly <dh436v@intl.att.com>
2022-12-29 13:43:46 +0200

    Add test runner API support | POST /api/run-tests

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 // next.config.js
 const withCSS = require("@zeit/next-css");
+const path = require("path");
+
 module.exports = withCSS({
-  /* config options here */
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname,
+    TEST_PATH: path.join(__dirname, "tests"),
+    // this is a hack due to next js dev env (https://github.com/vercel/next.js/discussions/14341)
+    TEST_CONFIG_PATH: path.join(__dirname, "utils/playwright-runner/playwright.config.ts"),
+  }
 });
